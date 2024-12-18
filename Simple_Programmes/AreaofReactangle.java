e26f374789d5807f54123d3ec9d1cf24ed10d4ba
Prasad Shenavi <140106431+prasadshenavi08@users.noreply.github.com>
2024-12-18 15:18:33 +0530

    Create AreaofReactangle.java
    Finds the Area of Reactangle

---
+++ Simple_Programmes/AreaofReactangle.java
@@ -0,0 +1,9 @@
+class AreaofReactangle{
+	public static void main(String [] args){
+		int len  = 15;
+		int bre = 30;
+		int area = len*bre;
+		
+		System.out.println("Area of Reactangle = "+ area);
+	}
+}
