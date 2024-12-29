cd4f638c24b8af3195dff79d6703b3aeb7ebbe2e
Prasad Shenavi <140106431+prasadshenavi08@users.noreply.github.com>
2024-12-29 12:58:37 +0530

    Create AbFood.java

---
+++ OOPsProgrammes/AbFood.java
@@ -0,0 +1,9 @@
+
+public abstract class AbFood {
+	public abstract void preapre();
+	
+	//Concrete Method or non Abstract Method
+	public static void sandwich1() {
+		System.out.println("Made sandwich");
+	}
+}
