e1bfa5c4875ed39dae7236cb4c94c816453ddca0
Prasad Shenavi <140106431+prasadshenavi08@users.noreply.github.com>
2025-01-19 15:03:18 +0530

    Create Demo1.java

---
+++ GUI/Demo1.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		JFrame f1 =new JFrame();
+		f1.setSize(400,500);
+		f1.setVisible(true);
+		f1.setTitle("My First GUI");
+	}
+}
